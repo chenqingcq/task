25b463768dac9c826793c61651dcbfe6bebc28de
jinian <1312006778@qq.com>
2018-03-28 11:34:54 +0800

    add comment.vue

--- src/router/test/comments.vue
+++ src/router/test/comments.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="b-LR-10">
+  <div class="b-LR-10 comment-container">
     <div class="panel b-MT-10 ">
       <div class="b-LR-10 b-T-5 between">
         <p class="middle b_FS-14"><span class="dot success"></span><span class="b-L-4 c_6 b_FS-14">审批留言</span></p>
-        <div class=" b_FS-10 c_7">
-          评论
+        <div class=" b_FS-10 c_7 commenthint">
+          评论<img class="comments-icon" :src='commentImgUrl' />
         </div>
       </div>
       <scroll class="comments-container" :height='height'>
         <ul class="comment-panel">
           <li v-for="(item,index) in items" :key="index" class="lisItem">
             <div class="left">
-              <img :src="item.imgUrl" alt="icon" class='icon'>
+              <div class="icon">
+                <!--<img :src="item.imgUrl" alt="icon" class='iconImg'>-->
+              </div>
             </div>
-            <div class="right"></div>
-            {{item.title}}
+            <div class="right">
+              <div class="head">
+                <div class="role b_FS-12 ">{{item.name}}({{item.role}})</div>
+                <div class="time-panel"><span>{{item.date}}</span><span>{{item.time}}</span></div>
+              </div>
+              <div class="comments-item">{{item.comments}}</div>
+            </div>
+            <i class="footbar"></i>
           </li>
         </ul>
       </scroll>
@@ -28,20 +36,46 @@
     data() {
       return {
         items: [{
-          title: '1',
-          imgUrl: require('@/assets/img/icon-book.png')
-        }, {
-          title: '1',
-          imgUrl: require('@/assets/img/icon-book.png')
-        }, {
-          title: '1',
-          imgUrl: require('@/assets/img/icon-book.png')
-        }],
-        height: 140*2+"px"
+            name: '张三',
+            role: '发布者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/logo.png')
+          }, {
+            name: '李四',
+            role: '执行者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/logo.png')
+          }, {
+            name: '王五',
+            role: '观察者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/logo.png')
+          },
+          {
+            name: '小六',
+            role: '观察者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/logo.png')
+          }
+        ],
+        height: 140 * 2 + "px"
       }
     },
     components: {
       scroll
+    },
+    computed: {
+      commentImgUrl() {
+        return require('@/assets/img/icon-comment.png')
+      }
     },
     methods: {
       dialog() {
@@ -52,23 +86,88 @@
 
 </script>
 <style lang="less" scoped>
+  .comment-container {
+    background: #fff;
+  }
+
+  .commenthint {
+    display: flex;
+    align-items: center;
+    .comments-icon {
+      display: inline-block;
+      width: 12*2px;
+      height: 12px*2;
+      margin-left: 2px;
+    }
+  }
+
   .comments-container {
     overflow: hidden;
     .lisItem {
+      width: 100%;
       height: 70*2px;
       display: flex;
-      border-bottom: 1px solid #979797;
+      position: relative;
+      .footbar {
+        display: inline-block;
+        position: absolute;
+        left: 22*2px;
+        right: 21*2px;
+        bottom: 0;
+        height: 2px;
+        background: rgba(151, 151, 151, .21);
+      }
       .left {
         width: 64*2px;
         height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
-        img {
+        .icon {
+          background: #D8D8D8;
           display: inline-block;
           width: 43px*2;
           height: 43px*2;
           border-radius: 50%;
+          display: flex;
+          border: 1*2px solid #14C9FE;
+          justify-content: center;
+          align-items: center;
+        }
+        img {
+          display: inline-block;
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+      }
+      .right {
+        flex: 1;
+        .head {
+          margin-top: 19*2px;
+          height: 17*2px;
+          .role {
+            color: #07A5FF;
+            display: flex;
+            font-size: 24px;
+            align-items: center;
+            display: inline-block;
+          }
+          .time-panel {
+            height: 100%;
+            color: #666;
+            float: right;
+            font-size: 16px;
+            min-width: 94*2px;
+            display: inline-block;
+            span {
+              margin-left: 5px;
+            }
+          }
+        }
+        .comments-item {
+          color: #666;
+          font-size: 20px;
         }
       }
     }
