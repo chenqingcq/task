3d416e1f39b38e0a37904b4e46033fe40f50aa34
jinian <1312006778@qq.com>
2018-03-29 17:55:44 +0800

    modified app

---
+++ src/views/comments/comments.vue
@@ -0,0 +1,174 @@
+i<template>
+  <div class="b-LR-10 comment-container">
+    <div class="panel b-MT-10 " style="background:#fff">
+      <div class="b-LR-10 b-T-5 between">
+        <p class="middle b_FS-14"><span class="dot success"></span><span class="b-L-4 c_6 b_FS-14">审批留言</span></p>
+        <div class=" b_FS-10 c_7 commenthint">
+          评论<img class="comments-icon" :src='commentImgUrl' />
+        </div>
+      </div>
+      <scroll class="comments-container" :height='height' >
+        <ul class="comment-panel" v-if="items.length>1">
+          <li v-for="(item,index) in items" :key="index" class="lisItem">
+            <div class="left" >
+              <!--测试-->
+              <div class="icon">
+                <img :src="item.imgUrl" alt="icon" class='iconImg'>
+              </div>
+            </div>
+            <div class="right">
+              <!--测试-->
+              <div class="head">
+                <div class="role b_FS-12 " >{{item.name}}({{item.role}})</div>
+                <div class="time-panel" ><span>{{item.date}}</span><span>{{item.time}}</span></div>
+              </div>
+              <div class="comments-item" >{{item.comments}}</div>
+            </div>
+            <i class="footbar"></i>
+          </li>
+        </ul>
+      </scroll>
+    </div>
+  </div>
+</template>
+<script>
+  import scroll from '@/common/base/scroll/scroll'
+  export default {
+    name: 'comments',
+    data() {
+      return {
+        items: [{
+            name: '张三',
+            role: '发布者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/img/icon-nominee.png')
+          }, {
+            name: '李四',
+            role: '执行者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/img/icon-nominee.png')
+          }, {
+            name: '王五',
+            role: '观察者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/img/icon-nominee.png')
+          },
+          {
+            name: '小六',
+            role: '观察者',
+            date: 'x年x月x日',
+            time: '2018.10.25',
+            comments: '会管家app',
+            imgUrl: require('@/assets/img/icon-nominee.png')
+          }
+        ],
+        height: 140 * 2 + "px"
+      }
+    },
+    components: {
+      scroll
+    },
+    computed: {
+      commentImgUrl() {
+        return require('@/assets/img/icon-comment.png')
+      }
+    },
+    methods: {
+      
+    }
+  }
+
+</script>
+<style lang="less" scoped>
+
+
+  .commenthint {
+    display: flex;
+    align-items: center;
+    .comments-icon {
+      display: inline-block;
+      width: 12*2px;
+      height: 12px*2;
+      margin-left: 4px;
+    }
+  }
+
+  .comments-container {
+    overflow: hidden;
+    .lisItem {
+      width: 100%;
+      height: 70*2px;
+      display: flex;
+      position: relative;
+      .footbar {
+        display: inline-block;
+        position: absolute;
+        left: 22*2px;
+        right: 21*2px;
+        bottom: 0;
+        height: 2px;
+        background: rgba(151, 151, 151, .21);
+      }
+      .left {
+        width: 64*2px;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .icon {
+          background: #D8D8D8;
+          display: inline-block;
+          width: 43px*2;
+          height: 43px*2;
+          border-radius: 50%;
+          display: flex;
+          border: 1*2px solid #14C9FE;
+          justify-content: center;
+          align-items: center;
+        }
+        img {
+          display: inline-block;
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+      }
+      .right {
+        flex: 1;
+        .head {
+          margin-top: 19*2px;
+          height: 17*2px;
+          .role {
+            color: #07A5FF;
+            display: flex;
+            font-size: 24px;
+            align-items: center;
+            display: inline-block;
+          }
+          .time-panel {
+            height: 100%;
+            color: #666;
+            float: right;
+            font-size: 16px;
+            min-width: 94*2px;
+            display: inline-block;
+            span {
+              margin-left: 5px;
+            }
+          }
+        }
+        .comments-item {
+          color: #666;
+          font-size: 20px;
+        }
+      }
+    }
+  }
+
+</style>
--- src/views/task/taskDetail/taskDetail.vue
+++ src/views/task/taskDetail/taskDetail.vue
@@ -1,20 +1,85 @@
-<style>
+<style lang="less" scoped>
+  .task-detail-slide {
+    padding: 4*2px 10*2px 0 10*2px;
+    .panel {
+      height: 345*2px;
+      padding: 10*2px;
+      .task-slide-top {
+        height: 57*2px;
+        display: flex;
+        .user-icon {
+          width: 57*2px;
+          height: 57*2px;
+          background: gray;
+          border-radius: 50%;
+        }
+        .right {
+          flex: 1;
+          margin-left: 15*2px;
+          .task-name {
+            font-size: 14*2px;
+            color: #666666;
+          }
+          .task-detail {
+            font-size: 12px*2;
+            color: #666666;
+            letter-spacing: 0;
+          }
+
+        }
+      }
+      .task-progress {
+        width: 100%;
+        height: 50*2px;
+        display: flex;
+        width:100%;
+        justify-content: center;
+      }
+    }
+  }
 
 </style>
 <template>
-    <div>
-        任务详情
+  <div class="task-detail-container">
+    <div class="task-detail-slide">
+      <div class="panel c-1 c_white-bg">
+        <div class="task-slide-top">
+          <div class="user-icon"></div>
+          <div class="right">
+            <p class="task-name">任务名名称</p>
+            <p class="task-detail">任务详情</p>
+            <p class="progress">
+              <div class="b_status pending">进行中</div>
+            </p>
+          </div>
+        </div>
+        <div class="task-slide-img"></div>
+        <div class="task-progress">
+          <p class="task-desc">展台基础已布置完毕</p>
+          <div class="btn-warp b-MT-10">
+            <div class="btn-small-success">
+              .btn-small-success
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="detail-progress"></div>
+    <div class="project-party"></div>
+    <comments></comments>
+  </div>
 </template>
 <script>
-    export default{
-        data(){
-            return{
+  import comments from '@/views/comments/comments'
+  export default {
+    data() {
+      return {
 
-            }
-        },
-        components:{
-
-        }
+      }
+    },
+    components: {
+      comments
     }
+  }
+
 </script>
