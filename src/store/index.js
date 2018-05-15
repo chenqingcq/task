c7eda094664837f5e139e6c30e041c0d424e02ac
雪山飞狐 <760643763@qq.com>
2018-05-15 18:06:08 +0800

    task

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 // 用于 解决页面刷新后的vuex 内存对象数据丢失的问题
-// import createPersist, { createStorage } from 'vuex-localstorage'
+import createPersist, { createStorage } from 'vuex-localstorage'
 
 import * as actions from './actions'
 import * as getters from './getters'
@@ -27,20 +27,20 @@ const store = new Vuex.Store({
     permission
   },
   strict: debug,
-  // plugins: debug ?
-  //   [createLogger(),createPersist({
-  //     namespace: 'namespace-for-state',
-  //     initialState: {},
-  //     // ONE_WEEK
-  //     expires: 7 * 24 * 60 * 60 * 1e3
-  //   })]
-  //   :
-  //   [createPersist({
-  //     namespace: 'namespace-for-state',
-  //     initialState: {},
-  //     // 一天后自动消失 one_Day
-  //     expires:  24 * 60 * 60 * 1e3
-  //   })]
+  plugins: debug ?
+    [createLogger(),createPersist({
+      namespace: 'namespace-for-state',
+      initialState: {},
+      // ONE_WEEK
+      expires: 7 * 24 * 60 * 60 * 1e3
+    })]
+    :
+    [createPersist({
+      namespace: 'namespace-for-state',
+      initialState: {},
+      // 一天后自动消失 one_Day
+      expires:  24 * 60 * 60 * 1e3
+    })]
 })
 
 if (module.hot) {
